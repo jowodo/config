02fe466cb8d3aef95a77258e3b5d0a01b3a6b2fa
pur80a <ziffernsumme11@gmail.com>
2022-10-04 16:44:29 +0200

    move.sh

---
+++ bin/move.sh
@@ -0,0 +1,47 @@
+#!/bin/bash 
+
+USAGE="USAGE: move {up|down} <FROM> <TO> <BY> [ PREFIX [ POSTFIX ] ]"
+
+if [ $# -lt 4 ] ; 
+then
+    echo $USAGE
+    exit 1
+fi
+
+DIRECTION=$1
+FROM="$2"
+TO="$3"
+BY=$4
+PREFIX=""
+POSTFIX=""
+DIGITS=${#TO}
+    
+#echo $FROM $TO
+
+if [ $1 == "up" ] 
+then 
+#    for (( i="$TO" ; i>="$FROM" ; i-- )) 
+for (( i=$(echo $TO | bc) ; i>=$( echo $FROM | bc) ; i-- )) 
+    do
+#        echo $i
+        from=${PREFIX}$( printf "%0${DIGITS}d" $i )${POSTFIX}
+        to=${PREFIX}$( printf "%0${DIGITS}d" $((i+${BY})) )${POSTFIX}
+#        echo $from $to 
+#        mv --no-clobber ${PREFIX}${i}${POSTFIX} ${PREFIX}$(($i+${BY}))${POSTFIX}
+        mv --no-clobber $from $to 
+    done
+elif [ $1 == "down" ] 
+then 
+    for (( i=$( echo $FROM| bc) ; i<=$(echo $TO | bc) ; i++ )) 
+    do
+#        mv --no-clobber ${PREFIX}${i}${POSTFIX} ${PREFIX}$(($i-${BY}))${POSTFIX}
+        from=${PREFIX}$( printf "%0${DIGITS}d" $i )${POSTFIX}
+        to=${PREFIX}$( printf "%0${DIGITS}d" $((i-${BY})) )${POSTFIX}
+        mv --no-clobber $from $to 
+    done
+else 
+    echo $USAGE
+    exit 1
+fi 
+
+
